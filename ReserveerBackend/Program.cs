fe017e2b6499dcaa068c3c42b05749e20161d1ae
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2018-05-13 23:08:42 +0200

    IP fix hopefully

--- ReserveerBackend/Program.cs
+++ ReserveerBackend/Program.cs
@@ -29,6 +29,7 @@ namespace ReserveerBackend
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
                 .Build();
     }
 }
