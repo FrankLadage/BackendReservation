b67a1ad5e5be68d21e43c17471d913ee6e6a5660
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2018-05-23 21:58:53 +0200

    Added admin shutdown

--- ReserveerBackend/Controllers/ShutdownController.cs
+++ ReserveerBackend/Controllers/ShutdownController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,14 @@ namespace ReserveerBackend.Controllers
         {
             if (secretkey == key)
                 Environment.Exit(0);
+        }
+
+        [HttpPost]
+        [Route("AdminShutdown")]
+        [Authorize(Roles = Authorization.AdminOrHigher)]
+        public void Shutdown()
+        {
+            Environment.Exit(0);
         }
 
         public static string secretkey { get; set; }
