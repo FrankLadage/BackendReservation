5553a3ed3d91d1f891217d3f126edebfecae43ed
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2018-06-19 22:56:43 +0200

    Added newest frontend

--- ReserveerBackend/wwwroot/assets/test-support.js
+++ ReserveerBackend/wwwroot/assets/test-support.js
@@ -10357,6 +10357,79 @@ define('@ember/test-helpers/wait-until', ['exports', '@ember/test-helpers/-utils
     });
   }
 });
+define('ember-basic-dropdown/test-support/helpers', ['exports', '@ember/test-helpers'], function (exports, _testHelpers) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.nativeTap = nativeTap;
+  exports.clickTrigger = clickTrigger;
+  exports.tapTrigger = tapTrigger;
+  exports.fireKeydown = fireKeydown;
+
+  exports.default = function () {
+    Ember.Test.registerAsyncHelper('clickDropdown', function (app, cssPath, options = {}) {
+      (true && !(false) && Ember.deprecate('Using the global `clickDropdown` acceptance helper from ember-basic-dropdown is deprecated. Please, explicitly import the `clickTrigger` or just use `click` helper from `@ember/test-helpers`.', false, { until: '1.0.0', id: 'ember-basic-dropdown-click-dropdown' }));
+
+      clickTrigger(cssPath, options);
+    });
+
+    Ember.Test.registerAsyncHelper('tapDropdown', function (app, cssPath, options = {}) {
+      (true && !(false) && Ember.deprecate('Using the global `tapDropdown` acceptance helper from ember-basic-dropdown is deprecated. Please, explicitly import the `tapTrigger` or just use `tap` helper from `@ember/test-helpers`.', false, { until: '1.0.0', id: 'ember-basic-dropdown-click-dropdown' }));
+
+      tapTrigger(cssPath, options);
+    });
+  };
+
+  function nativeTap(selector, options = {}) {
+    let touchStartEvent = new window.Event('touchstart', { bubbles: true, cancelable: true, view: window });
+    Object.keys(options).forEach(key => touchStartEvent[key] = options[key]);
+    Ember.run(() => document.querySelector(selector).dispatchEvent(touchStartEvent));
+    let touchEndEvent = new window.Event('touchend', { bubbles: true, cancelable: true, view: window });
+    Object.keys(options).forEach(key => touchEndEvent[key] = options[key]);
+    Ember.run(() => document.querySelector(selector).dispatchEvent(touchEndEvent));
+  }
+
+  function clickTrigger(scope, options = {}) {
+    let selector = '.ember-basic-dropdown-trigger';
+    if (scope) {
+      let element = document.querySelector(scope);
+      if (element.classList.contains('ember-basic-dropdown-trigger')) {
+        selector = scope;
+      } else {
+        selector = scope + ' ' + selector;
+      }
+    }
+    (0, _testHelpers.click)(selector, options);
+    return (0, _testHelpers.settled)();
+  }
+
+  function tapTrigger(scope, options = {}) {
+    let selector = '.ember-basic-dropdown-trigger';
+    if (scope) {
+      selector = scope + ' ' + selector;
+    }
+    nativeTap(selector, options);
+  }
+
+  function fireKeydown(selector, k) {
+    let oEvent = document.createEvent('Events');
+    oEvent.initEvent('keydown', true, true);
+    Ember.merge(oEvent, {
+      view: window,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      keyCode: k,
+      charCode: k
+    });
+    Ember.run(() => document.querySelector(selector).dispatchEvent(oEvent));
+  }
+
+  // acceptance helpers
+});
 define('ember-cli-qunit', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
   'use strict';
 
@@ -10521,6 +10594,268 @@ define('ember-intl/test-support/index', ['exports', '@ember/test-helpers', 'embe
     let { owner } = (0, _testHelpers.getContext)();
 
     return (0, _t2.default)(owner, ...args);
+  }
+});
+define('ember-power-select/test-support/helpers', ['exports', '@ember/test-helpers', 'ember-power-select/test-support/index'], function (exports, _testHelpers, _index) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.findContains = findContains;
+  exports.nativeMouseDown = nativeMouseDown;
+  exports.nativeMouseUp = nativeMouseUp;
+  exports.triggerKeydown = triggerKeydown;
+  exports.typeInSearch = typeInSearch;
+  exports.clickTrigger = clickTrigger;
+  exports.nativeTouch = nativeTouch;
+  exports.touchTrigger = touchTrigger;
+  exports.selectChoose = selectChoose;
+  exports.selectSearch = selectSearch;
+  exports.removeMultipleOption = removeMultipleOption;
+  exports.clearSelected = clearSelected;
+
+  exports.default = function () {
+    Ember.Test.registerAsyncHelper('selectChoose', function (_, cssPathOrTrigger, valueOrSelector, optionIndex) {
+      (true && !(true) && Ember.deprecate('Using the implicit global async helper `selectChoose` is deprecated. Please, import it explicitly with `import { selectChoose } from "ember-power-select/test-support"`', true, { id: 'ember-power-select-global-select-choose', until: '2.0.0' }));
+
+      return (0, _index.selectChoose)(cssPathOrTrigger, valueOrSelector, optionIndex);
+    });
+
+    Ember.Test.registerAsyncHelper('selectSearch', async function (app, cssPathOrTrigger, value) {
+      (true && !(true) && Ember.deprecate('Using the implicit global async helper `selectSearch` is deprecated. Please, import it explicitly with `import { selectSearch } from "ember-power-select/test-support"`', true, { id: 'ember-power-select-global-select-search', until: '2.0.0' }));
+
+      return (0, _index.selectSearch)(cssPathOrTrigger, value);
+    });
+
+    Ember.Test.registerAsyncHelper('removeMultipleOption', async function (app, cssPath, value) {
+      (true && !(true) && Ember.deprecate('Using the implicit global async helper `removeMultipleOption` is deprecated. Please, import it explicitly with `import { removeMultipleOption } from "ember-power-select/test-support"`', true, { id: 'ember-power-select-global-remove-multiple-option', until: '2.0.0' }));
+
+      return (0, _index.removeMultipleOption)(cssPath, value);
+    });
+
+    Ember.Test.registerAsyncHelper('clearSelected', async function (app, cssPath) {
+      (true && !(true) && Ember.deprecate('Using the implicit global async helper `clearSelected` is deprecated. Please, import it explicitly with `import { clearSelected } from "ember-power-select/test-support"`', true, { id: 'ember-power-select-global-clear-selected', until: '2.0.0' }));
+
+      return (0, _index.clearSelected)(cssPath);
+    });
+  };
+
+  /**
+   * @private
+   * @param {String} selector CSS3 selector of the elements to check the content
+   * @param {String} text Substring that the selected element must contain
+   * @returns HTMLElement The first element that maches the given selector and contains the
+   *                      given text
+   */
+  function findContains(selector, text) {
+    return [].slice.apply(document.querySelectorAll(selector)).filter(e => {
+      return e.textContent.trim().indexOf(text) > -1;
+    })[0];
+  }
+
+  async function nativeMouseDown(selectorOrDomElement, options) {
+    return (0, _testHelpers.triggerEvent)(selectorOrDomElement, 'mousedown', options);
+  }
+
+  async function nativeMouseUp(selectorOrDomElement, options) {
+    return (0, _testHelpers.triggerEvent)(selectorOrDomElement, 'mouseup', options);
+  }
+
+  async function triggerKeydown(domElement, k) {
+    return (0, _testHelpers.triggerKeyEvent)(domElement, 'keydown', k);
+  }
+
+  function typeInSearch(scopeOrText, text) {
+    let scope = '';
+
+    if (typeof text === 'undefined') {
+      text = scopeOrText;
+    } else {
+      scope = scopeOrText;
+    }
+
+    let selectors = ['.ember-power-select-search-input', '.ember-power-select-search input', '.ember-power-select-trigger-multiple-input', 'input[type="search"]'].map(selector => `${scope} ${selector}`).join(', ');
+
+    return (0, _testHelpers.fillIn)(selectors, text);
+  }
+
+  async function clickTrigger(scope, options = {}) {
+    let selector = '.ember-power-select-trigger';
+    if (scope) {
+      selector = `${scope} ${selector}`;
+    }
+    return (0, _testHelpers.click)(selector, options);
+  }
+
+  async function nativeTouch(selectorOrDomElement) {
+    (0, _testHelpers.triggerEvent)(selectorOrDomElement, 'touchstart');
+    return (0, _testHelpers.triggerEvent)(selectorOrDomElement, 'touchend');
+  }
+
+  async function touchTrigger() {
+    return nativeTouch('.ember-power-select-trigger');
+  }
+
+  async function selectChoose(cssPathOrTrigger, valueOrSelector, optionIndex) {
+    return (0, _index.selectChoose)(cssPathOrTrigger, valueOrSelector, optionIndex);
+  }
+
+  async function selectSearch(cssPathOrTrigger, value) {
+    return (0, _index.selectSearch)(cssPathOrTrigger, value);
+  }
+
+  async function removeMultipleOption(cssPath, value) {
+    return (0, _index.removeMultipleOption)(cssPath, value);
+  }
+
+  async function clearSelected(cssPath) {
+    return (0, _index.clearSelected)(cssPath);
+  }
+
+  // Helpers for acceptance tests
+});
+define('ember-power-select/test-support/index', ['exports', '@ember/test-helpers'], function (exports, _testHelpers) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.selectChoose = selectChoose;
+  exports.selectSearch = selectSearch;
+  exports.removeMultipleOption = removeMultipleOption;
+  exports.clearSelected = clearSelected;
+  async function selectChoose(cssPathOrTrigger, valueOrSelector, optionIndex) {
+    let trigger, target;
+    if (cssPathOrTrigger instanceof HTMLElement) {
+      if (cssPathOrTrigger.classList.contains('ember-power-select-trigger')) {
+        trigger = cssPathOrTrigger;
+      } else {
+        trigger = cssPathOrTrigger.querySelector('.ember-power-select-trigger');
+      }
+    } else {
+      trigger = document.querySelector(`${cssPathOrTrigger} .ember-power-select-trigger`);
+
+      if (!trigger) {
+        trigger = document.querySelector(cssPathOrTrigger);
+      }
+
+      if (!trigger) {
+        throw new Error(`You called "selectChoose('${cssPathOrTrigger}', '${valueOrSelector}')" but no select was found using selector "${cssPathOrTrigger}"`);
+      }
+    }
+
+    if (trigger.scrollIntoView) {
+      trigger.scrollIntoView();
+    }
+
+    let contentId = `${trigger.attributes['aria-owns'].value}`;
+    let content = document.querySelector(`#${contentId}`);
+    // If the dropdown is closed, open it
+    if (!content || content.classList.contains('ember-basic-dropdown-content-placeholder')) {
+      await (0, _testHelpers.click)(trigger);
+      await (0, _testHelpers.settled)();
+    }
+
+    // Select the option with the given text
+    let options = document.querySelectorAll(`#${contentId} .ember-power-select-option`);
+    let potentialTargets = [].slice.apply(options).filter(opt => opt.textContent.indexOf(valueOrSelector) > -1);
+    if (potentialTargets.length === 0) {
+      potentialTargets = document.querySelectorAll(`#${contentId} ${valueOrSelector}`);
+    }
+    console.log("PT", potentialTargets);
+    if (potentialTargets.length > 1) {
+      let filteredTargets = [].slice.apply(potentialTargets).filter(t => t.textContent.trim() === valueOrSelector);
+      if (optionIndex === undefined) {
+        target = filteredTargets[0] || potentialTargets[0];
+      } else {
+        target = filteredTargets[optionIndex] || potentialTargets[optionIndex];
+      }
+    } else {
+      target = potentialTargets[0];
+    }
+    if (!target) {
+      throw new Error(`You called "selectChoose('${cssPathOrTrigger}', '${valueOrSelector}')" but "${valueOrSelector}" didn't match any option`);
+    }
+    await (0, _testHelpers.click)(target);
+    return (0, _testHelpers.settled)();
+  }
+
+  async function selectSearch(cssPathOrTrigger, value) {
+    let trigger;
+    if (cssPathOrTrigger instanceof HTMLElement) {
+      trigger = cssPathOrTrigger;
+    } else {
+      let triggerPath = `${cssPathOrTrigger} .ember-power-select-trigger`;
+      trigger = document.querySelector(triggerPath);
+      if (!trigger) {
+        triggerPath = cssPathOrTrigger;
+        trigger = document.querySelector(triggerPath);
+      }
+
+      if (!trigger) {
+        throw new Error(`You called "selectSearch('${cssPathOrTrigger}', '${value}')" but no select was found using selector "${cssPathOrTrigger}"`);
+      }
+    }
+
+    if (trigger.scrollIntoView) {
+      trigger.scrollIntoView();
+    }
+
+    let contentId = `${trigger.attributes['aria-owns'].value}`;
+    let isMultipleSelect = !!trigger.querySelector('.ember-power-select-trigger-multiple-input');
+
+    let content = document.querySelector(`#${contentId}`);
+    let dropdownIsClosed = !content || content.classList.contains('ember-basic-dropdown-content-placeholder');
+    if (dropdownIsClosed) {
+      await (0, _testHelpers.click)(trigger);
+      await (0, _testHelpers.settled)();
+    }
+    let isDefaultSingleSelect = !!document.querySelector('.ember-power-select-search-input');
+
+    if (isMultipleSelect) {
+      await (0, _testHelpers.fillIn)(trigger.querySelector('.ember-power-select-trigger-multiple-input'), value);
+    } else if (isDefaultSingleSelect) {
+      await (0, _testHelpers.fillIn)('.ember-power-select-search-input', value);
+    } else {
+      // It's probably a customized version
+      let inputIsInTrigger = !!trigger.querySelector('.ember-power-select-trigger input[type=search]');
+      if (inputIsInTrigger) {
+        await (0, _testHelpers.fillIn)(trigger.querySelector('input[type=search]'), value);
+      } else {
+        await (0, _testHelpers.fillIn)(`#${contentId} .ember-power-select-search-input[type=search]`, 'input');
+      }
+    }
+    return (0, _testHelpers.settled)();
+  }
+
+  async function removeMultipleOption(cssPath, value) {
+    let elem;
+    let items = document.querySelectorAll(`${cssPath} .ember-power-select-multiple-options > li`);
+    let item = [].slice.apply(items).find(el => el.textContent.indexOf(value) > -1);
+    if (item) {
+      elem = item.querySelector('.ember-power-select-multiple-remove-btn');
+    }
+    try {
+      await (0, _testHelpers.click)(elem);
+      return (0, _testHelpers.settled)();
+    } catch (e) {
+      (true && Ember.warn('css path to remove btn not found'));
+
+      throw e;
+    }
+  }
+
+  async function clearSelected(cssPath) {
+    let elem = document.querySelector(`${cssPath} .ember-power-select-clear-btn`);
+    try {
+      await (0, _testHelpers.click)(elem);
+      return (0, _testHelpers.settled)();
+    } catch (e) {
+      (true && Ember.warn('css path to clear btn not found'));
+
+      throw e;
+    }
   }
 });
 define('ember-qunit/adapter', ['exports', 'qunit', '@ember/test-helpers/has-ember-version'], function (exports, _qunit, _hasEmberVersion) {
